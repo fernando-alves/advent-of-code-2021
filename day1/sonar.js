539862ccecf75f6a8921739aa5d9bb754eb23fd6
Fernando <fernandoasfj+github@gmail.com>
2021-12-02 00:52:04 +0100

    Refactor inspired by ruby's enum api

--- day1/sonar.js
+++ day1/sonar.js
@@ -1,13 +1,10 @@
 const sum = (...measurements) => measurements.reduce((acc, measurement) => acc + measurement)
 
-const countIncrements = (measurements, windowLength) => {
-  const measurementsWithinWindow = measurements.slice(0, measurements.length - windowLength)
-  return measurementsWithinWindow.reduce((increments, _, currentIndex) => {
-    const currentWindow = measurements.slice(currentIndex, currentIndex + windowLength)
-    const nextWindow = measurements.slice(currentIndex + 1, currentIndex + windowLength + 1)
-    return sum(nextWindow) > sum(currentWindow) ? increments + 1 : increments
-  }, 0)
-}
+const windowsOfSize = (measurements, windowSize) => measurements.slice(0, measurements.length - windowSize + 1).map((_, index) => measurements.slice(index, index + windowSize))
+
+const eachPairOf = (windows) => windows.slice(0, windows.length - 1).map((_, index) => [windows[index], windows[index + 1]])
+
+const countIncrements = (measurements, windowSize) => eachPairOf(windowsOfSize(measurements, windowSize)).filter(consecutiveWindows => sum(consecutiveWindows[1]) > sum(consecutiveWindows[0])).length
 
 module.exports = {
   countIncrements: depthMeasurements => countIncrements(depthMeasurements, 1),
