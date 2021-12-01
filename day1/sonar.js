e9a4bccf99cf03a5ad3c5da5d7452c1e92a87b5f
Fernando <fernandoasfj+github@gmail.com>
2021-12-01 16:56:37 +0100

    Extract repeated code to a function

--- day1/sonar.js
+++ day1/sonar.js
@@ -3,11 +3,13 @@ const countIncrements = depthMeasurements => depthMeasurements.reduce((count, me
   return nextMeasurement && measurement < nextMeasurement ? count + 1 : count
 }, 0)
 
+const sum = (...measurements) => measurements.reduce((acc, measurement) => acc + measurement)
+
 const countSlidingWindowIncrements = depthMeasurements => depthMeasurements.slice(0, depthMeasurements.length - 3).reduce((count, _, measurementIndex) => {
   const slidingWindow = depthMeasurements.slice(measurementIndex, measurementIndex+3)
-  const slidingWindowCount = slidingWindow.reduce((acc, m) => acc+m)
+  const slidingWindowCount = sum(slidingWindow)
   const nextSlidingWindow = depthMeasurements.slice(measurementIndex+1, measurementIndex+4)
-  const nextSlidingWindowCount = nextSlidingWindow.reduce((acc, m) => acc+m)
+  const nextSlidingWindowCount = sum(nextSlidingWindow)
   return slidingWindowCount < nextSlidingWindowCount ? count + 1 : count
 }, 0)
 
