a52c0419ff2befca20fe1b6f44517f12a978dcf3
Fernando <fernandoasfj+github@gmail.com>
2021-12-01 18:08:36 +0100

    Parametrize solution so it can be reused for both cases

--- day1/sonar.js
+++ day1/sonar.js
@@ -1,16 +1,12 @@
-const countIncrements = depthMeasurements => depthMeasurements.slice(0, depthMeasurements.length - 1).reduce((count, measurement, measurementIndex) => {
-  const nextMeasurement = depthMeasurements[measurementIndex+1]
+const sum = (...measurements) => measurements.reduce((acc, measurement) => acc + measurement)
+
+const countIncrements = (measurements, windowLength) => measurements.slice(0, measurements.length - windowLength).reduce((count, _, measurementIndex) => {
+  const measurement = sum(measurements.slice(measurementIndex, measurementIndex + windowLength))
+  const nextMeasurement = sum(measurements.slice(measurementIndex + 1, measurementIndex + windowLength + 1))
   return measurement < nextMeasurement ? count + 1 : count
 }, 0)
 
-const sum = (...measurements) => measurements.reduce((acc, measurement) => acc + measurement)
-
-const countSlidingWindowIncrements = depthMeasurements => depthMeasurements.slice(0, depthMeasurements.length - 3).reduce((count, _, measurementIndex) => {
-  const slidingWindow = depthMeasurements.slice(measurementIndex, measurementIndex+3)
-  const slidingWindowCount = sum(slidingWindow)
-  const nextSlidingWindow = depthMeasurements.slice(measurementIndex+1, measurementIndex+4)
-  const nextSlidingWindowCount = sum(nextSlidingWindow)
-  return slidingWindowCount < nextSlidingWindowCount ? count + 1 : count
-}, 0)
-
-module.exports = { countIncrements, countSlidingWindowIncrements }
+module.exports = {
+  countIncrements: depthMeasurements => countIncrements(depthMeasurements, 1),
+  countSlidingWindowIncrements: depthMeasurements => countIncrements(depthMeasurements, 3)
+}
