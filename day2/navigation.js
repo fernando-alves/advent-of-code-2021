1997971b91d3d3e4943d9269186cfc1c23da614b
Fernando <fernandoasfj+github@gmail.com>
2021-12-02 13:23:42 +0100

    Improve instruction parsing

--- day2/navigation.js
+++ day2/navigation.js
@@ -1,32 +1,35 @@
+const toInstructionAndUnit = instructions => instructions.map(instruction => {
+  const [direction, unitAsText] = instruction.split(' ')
+  return {direction, unit: Number(unitAsText)}
+})
+
 const navigate = instructions =>
-  instructions.reduce((position, instruction) => {
-    const [direction, unit] = instruction.split(' ')
-    switch (direction) {
+toInstructionAndUnit(instructions).reduce((position, instruction) => {
+    switch (instruction.direction) {
       case 'forward':
-        position.horizontal = position.horizontal + Number(unit)
+        position.horizontal = position.horizontal + instruction.unit
         break
       case 'down':
-        position.depth = position.depth + Number(unit)
+        position.depth = position.depth + instruction.unit
         break
       case 'up':
-        position.depth = position.depth - Number(unit)
+        position.depth = position.depth - instruction.unit
     }
     return position
   }, {depth: 0, horizontal: 0})
 
 const aimedNavigate = instructions =>
-  instructions.reduce((position, instruction) => {
-    const [direction, unit] = instruction.split(' ')
-    switch (direction) {
+toInstructionAndUnit(instructions).reduce((position, instruction) => {
+    switch (instruction.direction) {
       case 'forward':
-        position.horizontal = position.horizontal + Number(unit)
-        position.depth = position.depth + (position.aim * Number(unit))
+        position.horizontal = position.horizontal + instruction.unit
+        position.depth = position.depth + (position.aim * instruction.unit)
         break
       case 'down':
-        position.aim = position.aim + Number(unit)
+        position.aim = position.aim + instruction.unit
         break
       case 'up':
-        position.aim = position.aim - Number(unit)
+        position.aim = position.aim - instruction.unit
     }
     return position
   }, {depth: 0, horizontal: 0, aim: 0})
