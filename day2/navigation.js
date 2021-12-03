ba2c68f7fa79a0a98315c94b8bbd32210a9dbe37
Fernando <fernandoasfj+github@gmail.com>
2021-12-03 18:10:13 +0100

    Shorten it a bit

--- day2/navigation.js
+++ day2/navigation.js
@@ -7,13 +7,13 @@ const navigate = instructions =>
 toInstructionAndUnit(instructions).reduce((position, instruction) => {
     switch (instruction.direction) {
       case 'forward':
-        position.horizontal = position.horizontal + instruction.unit
+        position.horizontal += instruction.unit
         break
       case 'down':
-        position.depth = position.depth + instruction.unit
+        position.depth += instruction.unit
         break
       case 'up':
-        position.depth = position.depth - instruction.unit
+        position.depth -= instruction.unit
     }
     return position
   }, {depth: 0, horizontal: 0})
@@ -22,14 +22,14 @@ const aimedNavigate = instructions =>
 toInstructionAndUnit(instructions).reduce((position, instruction) => {
     switch (instruction.direction) {
       case 'forward':
-        position.horizontal = position.horizontal + instruction.unit
-        position.depth = position.depth + (position.aim * instruction.unit)
+        position.horizontal += instruction.unit
+        position.depth += position.aim * instruction.unit
         break
       case 'down':
-        position.aim = position.aim + instruction.unit
+        position.aim += instruction.unit
         break
       case 'up':
-        position.aim = position.aim - instruction.unit
+        position.aim -= instruction.unit
     }
     return position
   }, {depth: 0, horizontal: 0, aim: 0})
